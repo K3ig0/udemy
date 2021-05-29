1d01b5720fb881feda36712c20bc95ff3c55ea71
Manu <manuel.sanchez.naveira@gmail.com>
2021-05-29 16:55:23 +0200

    new: JUnit 5 course - TDD 4 - last test passed

--- junit5_course/src/main/java/com/healthycoderapp/ActivityCalculator.java
+++ junit5_course/src/main/java/com/healthycoderapp/ActivityCalculator.java
@@ -6,6 +6,10 @@ public class ActivityCalculator {
 	
 	public static String rateActivityLevel(int weeklyCardioMin, int weeklyWorkoutSessions) {
 		
+		if (weeklyCardioMin < 0 || weeklyWorkoutSessions < 0) {
+			throw new RuntimeException("Input below 0");
+		}
+		
 		int totalMinutes = weeklyCardioMin + weeklyWorkoutSessions * WORKOUT_DURATION_MIN;
 		double avgDailyActivityMins = totalMinutes / 7.0;
 		
