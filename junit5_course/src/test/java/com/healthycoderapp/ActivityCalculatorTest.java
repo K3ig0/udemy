83e4d419bdf3542db78bf331a7ce010c74b36316
Manu <manuel.sanchez.naveira@gmail.com>
2021-05-29 16:46:26 +0200

    new: JUnit 5 course - TDD 2 - tests signatures

---
+++ junit5_course/src/test/java/com/healthycoderapp/ActivityCalculatorTest.java
@@ -0,0 +1,29 @@
+package com.healthycoderapp;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ActivityCalculatorTest {
+
+	@Test
+	void should_ReturnBad_When_AvgBelow20() {
+		
+	}
+	
+	@Test
+	void should_ReturnAvg_When_AvgBetween20And40() {
+		
+	}
+	
+	@Test
+	void should_ReturnGood_when_AvgAbove40() {
+		
+	}
+	
+	@Test
+	void should_ThrowException_When_InputBelowZero() {
+		
+	}
+
+}
